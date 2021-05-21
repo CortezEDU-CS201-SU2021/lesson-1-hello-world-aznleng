5eb8c50e4af509f5c890b19e41358e5185a0d723
Elaine Cortez <>
2021-05-21 01:26:38 +0000

    Assignment complete.

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 # The Hello World project!
 # Task 1: Print out "Hello World!"
-
+print("Hello World!")
 
 # Task 2: Create a variable "my_name" and assign it to your name. Print out "Hello {YOUR NAME}!" where {YOUR NAME} is replaced using the variable "my_name" and concatenation.
-
-
+my_name = "Jo"
+print("Hello " + my_name + "!")
 
 # Task 3: Take in an input and assign it to the variable "your_name". Print out "Hello {YOUR NAME}!" where {YOUR NAME} is replaced using the variable "your_name" and concatenation.
-
+your_name = input()
+print("Hello " + your_name + "!")
